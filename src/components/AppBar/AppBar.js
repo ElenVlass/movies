7996df95df3123ecf0fd60bb4de70691e7a54122
Elena Vlasenko <olefelena@gmail.com>
2021-07-02 12:18:00 +0300

    add logo

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from "./AppBar.module.scss";
+import logo from "../../images/logo.png";
 
 import Navigation from "../Navigation";
 
 const AppBar = () => (
   <header className={styles.AppBar}>
-    <Navigation />
+    {/* <Container> */}
+    <div className={styles.Navigation}>
+      <div className={styles.Logo}>
+        <img src={logo} alt="logo" width="60" />
+      </div>
+      <Navigation />
+    </div>
   </header>
 );
 
